3f0194006237b7aef5c5a334f03117f8fb45f383
niamhcahillfugro <n.cahill@fugro.com>
2023-01-25 15:21:34 +0800

    me deleting things

--- undefinedtest.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from 'react'
-import styles from './test.module.css'
-class Button_294 extends Component {
-render() {return (
-<div className={styles.Button_294}><div className={styles._Button___Element___Base_Default_678}><div className={styles.icon_left_866}><div className={styles.favorite_856}><div className={styles.Frame_326}><div className={styles.Vector_152}></div>
-</div></div></div><div className={styles.text_711}><div className={styles._Button___Element___Label_361}><p className={styles.text_507}>Button</p>
-</div></div><div className={styles.icon_right_282}><div className={styles.arrow_forward_368}><div className={styles.Frame_924}><div className={styles.Vector_534}></div>
-</div></div></div></div></div>)}}
-export default Button_294
