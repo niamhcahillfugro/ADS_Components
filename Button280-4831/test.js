3a4fd0b151646de523afb7c8cc40d512902c74eb
niamhcahillfugro <118794299+niamhcahillfugro@users.noreply.github.com>
2023-01-25 16:12:56 +0800

    creating a new folder with files

---
+++ Button280-4831/test.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+import styles from './test.module.css'
+class Button_462 extends Component {
+render() {return (
+<div className={styles.Button_462}><div className={styles._Button___Element___Base_Default_589}><div className={styles.icon_left_544}><div className={styles.favorite_504}><div className={styles.Frame_189}><div className={styles.Vector_631}></div>
+</div></div></div><div className={styles.text_668}><div className={styles._Button___Element___Label_279}><p className={styles.text_78}>Button</p>
+</div></div><div className={styles.icon_right_632}><div className={styles.arrow_forward_307}><div className={styles.Frame_506}><div className={styles.Vector_753}></div>
+</div></div></div></div></div>)}}
+export default Button_462
