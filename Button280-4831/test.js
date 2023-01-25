34c180b41b5f801fd941cdd92b281d3499f9b683
niamhcahillfugro <118794299+niamhcahillfugro@users.noreply.github.com>
2023-01-25 16:38:22 +0800

    deleted and added new react file

---
+++ Button280-4831/test.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+import styles from './test.module.css'
+class Button_420 extends Component {
+render() {return (
+<div className={styles.Button_420}><div className={styles._Button___Element___Base_Default_105}><div className={styles.icon_left_76}><div className={styles.favorite_734}><div className={styles.Frame_200}><div className={styles.Vector_527}></div>
+</div></div></div><div className={styles.text_511}><div className={styles._Button___Element___Label_479}><p className={styles.text_174}>Button</p>
+</div></div><div className={styles.icon_right_348}><div className={styles.arrow_forward_395}><div className={styles.Frame_802}><div className={styles.Vector_647}></div>
+</div></div></div></div></div>)}}
+export default Button_420
