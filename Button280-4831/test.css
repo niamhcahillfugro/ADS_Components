3bb90f3df0a7eb5bfcc9f7b43261fbb9b64c4091
niamhcahillfugro <118794299+niamhcahillfugro@users.noreply.github.com>
2023-01-25 16:26:24 +0800

    deleting this file

--- Button280-4831/test.css
+++
@@ -1,9 +0,0 @@
-import React, { Component } from 'react'
-import styles from './test.module.css'
-class Button_324 extends Component {
-render() {return (
-<div className={styles.Button_324}><div className={styles._Button___Element___Base_Default_746}><div className={styles.icon_left_977}><div className={styles.favorite_156}><div className={styles.Frame_411}><div className={styles.Vector_504}></div>
-</div></div></div><div className={styles.text_412}><div className={styles._Button___Element___Label_254}><p className={styles.text_175}>Button</p>
-</div></div><div className={styles.icon_right_695}><div className={styles.arrow_forward_963}><div className={styles.Frame_412}><div className={styles.Vector_110}></div>
-</div></div></div></div></div>)}}
-export default Button_324
