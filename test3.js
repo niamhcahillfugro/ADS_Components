58f8d37a5ec609a398c294dab89bdcb5e3feaf78
niamhcahillfugro <118794299+niamhcahillfugro@users.noreply.github.com>
2023-01-25 14:45:20 +0800

    testing updating a file

--- test3.js
+++ test3.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react'
 import styles from './test.module.css'
-class Button_630 extends Component {
+class Button_561 extends Component {
 render() {return (
-<div className={styles.Button_630}><div className={styles._Button___Element___Base_Default_333}><div className={styles.icon_left_526}><div className={styles.favorite_465}><div className={styles.Frame_160}><div className={styles.Vector_210}></div>
-</div></div></div><div className={styles.text_774}><div className={styles._Button___Element___Label_266}><p className={styles.text_499}>Button</p>
-</div></div><div className={styles.icon_right_366}><div className={styles.arrow_forward_217}><div className={styles.Frame_109}><div className={styles.Vector_600}></div>
+<div className={styles.Button_561}><div className={styles._Button___Element___Base_Default_563}><div className={styles.icon_left_289}><div className={styles.favorite_115}><div className={styles.Frame_594}><div className={styles.Vector_892}></div>
+</div></div></div><div className={styles.text_363}><div className={styles._Button___Element___Label_211}><p className={styles.text_519}>Button</p>
+</div></div><div className={styles.icon_right_769}><div className={styles.arrow_forward_466}><div className={styles.Frame_20}><div className={styles.Vector_923}></div>
 </div></div></div></div></div>)}}
-export default Button_630
+export default Button_561
