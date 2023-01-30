1a50a1a966ae48f3ef1a78e7ea8240d10e794c9f
niamhcahillfugro <118794299+niamhcahillfugro@users.noreply.github.com>
2023-01-30 12:38:23 +0800

    Uploaded Button280-4828 component files

--- Button280-4828/test.js
+++ Button280-4828/test.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react'
 import styles from './test.module.css'
-class Button_435 extends Component {
+class Button_104 extends Component {
 render() {return (
-<div className={styles.Button_435}><div className={styles._Button___Element___Base_Default_726}><div className={styles.icon_left_998}><div className={styles.favorite_845}><div className={styles.Frame_395}></div></div></div><div className={styles.text_476}><div className={styles._Button___Element___Label_130}><p className={styles.text_15}>Button</p>
-</div></div><div className={styles.icon_right_39}><div className={styles.arrow_forward_509}><div className={styles.Frame_680}><div className={styles.Vector_108}><svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+<div className={styles.Button_104}><div className={styles._Button___Element___Base_Default_528}><div className={styles.icon_left_635}><div className={styles.favorite_843}><div className={styles.Frame_155}></div></div></div><div className={styles.text_381}><div className={styles._Button___Element___Label_736}><p className={styles.text_319}>Button</p>
+</div></div><div className={styles.icon_right_110}><div className={styles.arrow_forward_172}><div className={styles.Frame_849}><div className={styles.Vector_466}><svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
 <path d="M7.5696 15.3417C7.4177 15.1729 7.34175 14.9662 7.34175 14.7215C7.34175 14.4767 7.4177 14.2784 7.5696 14.1265L13.0379 8.6582H0.860757C0.624471 8.6582 0.42194 8.57803 0.253164 8.4177C0.0843882 8.25736 0 8.05061 0 7.79744C0 7.54428 0.0843882 7.33753 0.253164 7.17719C0.42194 7.01686 0.624471 6.93669 0.860757 6.93669H13.0379L7.5696 1.46835C7.4177 1.31645 7.34175 1.11392 7.34175 0.860757C7.34175 0.607593 7.4177 0.396623 7.5696 0.227847C7.73837 0.075949 7.94512 0 8.18985 0C8.43457 0 8.63289 0.075949 8.78478 0.227847L15.7468 7.18985C15.8312 7.29112 15.8945 7.38816 15.9367 7.48099C15.9789 7.57382 16 7.6793 16 7.79744C16 7.89871 15.9789 7.99998 15.9367 8.10124C15.8945 8.20251 15.8312 8.30377 15.7468 8.40504L8.78478 15.367C8.63289 15.5189 8.43457 15.5907 8.18985 15.5822C7.94512 15.5738 7.73837 15.4936 7.5696 15.3417Z" fill="#111A22"/>
 </svg>
 </div>
 </div></div></div></div></div>)}}
-export default Button_435
+export default Button_104
