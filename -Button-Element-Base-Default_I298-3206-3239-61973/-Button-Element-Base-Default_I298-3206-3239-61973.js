5c7e5c19bcaff0a51b95f5618d6992d935d03a6a
niamhcahillfugro <118794299+niamhcahillfugro@users.noreply.github.com>
2023-02-03 16:41:41 +0800

    test

---
+++ -Button-Element-Base-Default_I298-3206-3239-61973/-Button-Element-Base-Default_I298-3206-3239-61973.js
@@ -0,0 +1,7 @@
+import React, { Component } from 'react'
+import styles from './test.module.css'
+class -Button-Element-Base-Default_I298-3206-3239-61973 extends Component {
+render() {return (
+<div className={styles.-Button-Element-Base-Default_I298-3206-3239-61973}><div className={styles.text_I298-3206-3239-61973-2504-64019}><div className={styles.-Button-Element-Label_I298-3206-3239-61973-2504-64020}><p className={styles.text_I298-3206-3239-61973-2504-64020-1087-15453}>Button</p>
+</div></div><div className={styles.icon-right_I298-3206-3239-61973-2504-64023}><div className={styles.arrow-forward_I298-3206-3239-61973-2504-64024}><div className={styles.Frame_I298-3206-3239-61973-2504-64024-5013-16067}></div></div></div></div>)}}
+export default -Button-Element-Base-Default_I298-3206-3239-61973
