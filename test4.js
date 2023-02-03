ad0239848572fd89e2c187ed43809e8c383ab6c0
niamhcahillfugro <n.cahill@fugro.com>
2023-02-03 16:17:01 +0800

    me refreshing

--- test4.js
+++
@@ -1,7 +0,0 @@
-import React, { Component } from 'react'
-import styles from './test.module.css'
-class Button extends Component {
-render() {return (
-<div className={styles.Button}><div className={styles._Button / Element / Base Default}><div className={styles.text}><div className={styles._Button / Element / Label}><p className={styles.text}>Button</p>
-</div></div><div className={styles.icon right}><div className={styles.arrow_forward}><div className={styles.Frame}></div></div></div></div></div>)}}
-export default Button
