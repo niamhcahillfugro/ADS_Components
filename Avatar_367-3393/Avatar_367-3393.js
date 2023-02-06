c803091e0a6d67b4a76e1e972b23ddff041365ef
niamhcahillfugro <118794299+niamhcahillfugro@users.noreply.github.com>
2023-02-06 15:39:56 +0800

    uploading react file Avatar_367-3393.js

---
+++ Avatar_367-3393/Avatar_367-3393.js
@@ -0,0 +1,8 @@
+import React, { Component } from 'react'
+import styles from './test.module.css'
+class Avatar_367-3393 extends Component {
+render() {return (
+<div className={styles.Avatar_367-3393}><div className={styles.background_I367-3393-2123-33614}></div>
+<p className={styles.Initials_I367-3393-2123-33615}>MW</p>
+</div>)}}
+export default Avatar_367-3393
