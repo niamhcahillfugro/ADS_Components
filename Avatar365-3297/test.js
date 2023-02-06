b5b8d6316b042db109d7c8dc3d904abf4c659273
niamhcahillfugro <118794299+niamhcahillfugro@users.noreply.github.com>
2023-02-06 15:27:54 +0800

    Uploaded Avatar365-3297 component files

---
+++ Avatar365-3297/test.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+import styles from './test.module.css'
+class Avatar_724 extends Component {
+render() {return (
+<div className={styles.Avatar_724}><div className={styles.background_171}></div>
+<p className={styles.Initials_140}>MW</p>
+<div className={styles.image_782}></div><div className={styles.Status_badge_383}><div className={styles.base_959}></div>
+</div></div>)}}
+export default Avatar_724
