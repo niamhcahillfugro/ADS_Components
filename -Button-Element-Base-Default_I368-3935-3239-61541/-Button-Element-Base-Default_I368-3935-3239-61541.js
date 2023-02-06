971d2c79ae51ee2d510c1746b450968d94c7af22
niamhcahillfugro <118794299+niamhcahillfugro@users.noreply.github.com>
2023-02-06 16:00:51 +0800

    uploading react file -Button-Element-Base-Default_I368-3935-3239-61541.js

---
+++ -Button-Element-Base-Default_I368-3935-3239-61541/-Button-Element-Base-Default_I368-3935-3239-61541.js
@@ -0,0 +1,7 @@
+import React, { Component } from 'react'
+import styles from './-Button-Element-Base-Default_I368-3935-3239-61541.css'
+class -Button-Element-Base-Default_I368-3935-3239-61541 extends Component {
+render() {return (
+<div className={styles.-Button-Element-Base-Default_I368-3935-3239-61541}><div className={styles.text_I368-3935-3239-61541-2504-64001}><div className={styles.-Button-Element-Label_I368-3935-3239-61541-2504-64002}><p className={styles.text_I368-3935-3239-61541-2504-64002-1087-15453}>Button</p>
+</div></div></div>)}}
+export default -Button-Element-Base-Default_I368-3935-3239-61541
