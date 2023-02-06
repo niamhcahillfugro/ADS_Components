26e3b50b20f6a509c7e175080c1f404ac4765091
niamhcahillfugro <118794299+niamhcahillfugro@users.noreply.github.com>
2023-02-06 13:06:49 +0800

    Uploaded Status badge97-3207 component files

---
+++ Status badge97-3207/test.js
@@ -0,0 +1,7 @@
+import React, { Component } from 'react'
+import styles from './test.module.css'
+class Status_badge_58 extends Component {
+render() {return (
+<div className={styles.Status_badge_58}><div className={styles.base_917}></div>
+</div>)}}
+export default Status_badge_58
