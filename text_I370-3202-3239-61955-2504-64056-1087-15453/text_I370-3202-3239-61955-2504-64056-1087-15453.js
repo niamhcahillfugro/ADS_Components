9b234c02e244238a6e062c1d8897240e80c72782
niamhcahillfugro <118794299+niamhcahillfugro@users.noreply.github.com>
2023-02-06 16:46:34 +0800

    uploading react file text_I370-3202-3239-61955-2504-64056-1087-15453.js

---
+++ text_I370-3202-3239-61955-2504-64056-1087-15453/text_I370-3202-3239-61955-2504-64056-1087-15453.js
@@ -0,0 +1,7 @@
+import React, { Component } from 'react'
+import styles from './text_I370-3202-3239-61955-2504-64056-1087-15453.css'
+class text_I370-3202-3239-61955-2504-64056-1087-15453 extends Component {
+render() {return (
+<p className={styles.text_I370-3202-3239-61955-2504-64056-1087-15453}>Button</p>
+)}}
+export default text_I370-3202-3239-61955-2504-64056-1087-15453
