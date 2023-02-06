17976ddeee2b06155f9913d063422b3014b79164
niamhcahillfugro <118794299+niamhcahillfugro@users.noreply.github.com>
2023-02-06 13:16:44 +0800

    Uploaded Numeric badge97-3266 component files

--- Numeric badge97-3266/test.js
+++ Numeric badge97-3266/test.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import styles from './test.module.css'
-class Numeric_badge_495 extends Component {
+class Numeric_badge_317 extends Component {
 render() {return (
-<div className={styles.Numeric_badge_495}><p className={styles.1_817}>1</p>
+<div className={styles.Numeric_badge_317}><p className={styles.1_141}>1</p>
 </div>)}}
-export default Numeric_badge_495
+export default Numeric_badge_317
