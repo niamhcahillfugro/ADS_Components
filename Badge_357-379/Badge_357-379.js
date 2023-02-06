acf7face4e5c20ab6c1297cb0ee1d5bf00feab72
niamhcahillfugro <118794299+niamhcahillfugro@users.noreply.github.com>
2023-02-06 12:08:19 +0800

    uploading react file Badge_357-379.js

---
+++ Badge_357-379/Badge_357-379.js
@@ -0,0 +1,6 @@
+import React, { Component } from 'react'
+import styles from './Badge_357-379.css'
+class Badge_357-379 extends Component {
+render() {return (
+[object Promise])}}
+export default Badge_357-379
