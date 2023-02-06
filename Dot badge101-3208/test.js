0b42994a57406c65f39707db1395999f569f1702
niamhcahillfugro <118794299+niamhcahillfugro@users.noreply.github.com>
2023-02-06 13:59:54 +0800

    Uploaded Dot badge101-3208 component files

---
+++ Dot badge101-3208/test.js
@@ -0,0 +1,7 @@
+import React, { Component } from 'react'
+import styles from './test.module.css'
+class Dot_badge_923 extends Component {
+render() {return (
+<div className={styles.Dot_badge_923}><div className={styles.dot_407}></div>
+</div>)}}
+export default Dot_badge_923
