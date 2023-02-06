54cdca11bad9eb2daf6b899c2d353d40186e589f
niamhcahillfugro <118794299+niamhcahillfugro@users.noreply.github.com>
2023-02-06 13:08:52 +0800

    Uploaded Status badge97-3207 component files

--- Status badge97-3207/test.js
+++ Status badge97-3207/test.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import styles from './test.module.css'
-class Status_badge_58 extends Component {
+class Status_badge_225 extends Component {
 render() {return (
-<div className={styles.Status_badge_58}><div className={styles.base_917}></div>
+<div className={styles.Status_badge_225}><div className={styles.base_525}></div>
 </div>)}}
-export default Status_badge_58
+export default Status_badge_225
