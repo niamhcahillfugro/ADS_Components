3f43ed5a445bfce974c5a6a5622ba1ba46045460
niamhcahillfugro <118794299+niamhcahillfugro@users.noreply.github.com>
2023-02-06 15:47:26 +0800

    uploading react file Button_368-3935.js

---
+++ Button_368-3935/Button_368-3935.js
@@ -0,0 +1,7 @@
+import React, { Component } from 'react'
+import styles from './Button_368-3935.css'
+class Button_368-3935 extends Component {
+render() {return (
+<div className={styles.Button_368-3935}><div className={styles.-Button-Element-Base-Default_I368-3935-3239-61541}><div className={styles.text_I368-3935-3239-61541-2504-64001}><div className={styles.-Button-Element-Label_I368-3935-3239-61541-2504-64002}><p className={styles.text_I368-3935-3239-61541-2504-64002-1087-15453}>Button</p>
+</div></div></div></div>)}}
+export default Button_368-3935
