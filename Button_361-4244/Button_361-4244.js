ecbe1534a387b3b2d31343359d79513eeb80597c
niamhcahillfugro <118794299+niamhcahillfugro@users.noreply.github.com>
2023-02-06 15:00:20 +0800

    uploading react file Button_361-4244.js

--- Button_361-4244/Button_361-4244.js
+++ Button_361-4244/Button_361-4244.js
@@ -2,6 +2,6 @@ import React, { Component } from 'react'
 import styles from './test.module.css'
 class Button_361-4244 extends Component {
 render() {return (
-<div className={styles.Button_361-4244}><div className={styles.-Button-Element-Base-Default_I361-4244-3239-61541}><div className={styles.text_I361-4244-3239-61541-2504-64001}><div className={styles.-Button-Element-Label_I361-4244-3239-61541-2504-64002}><p className={styles.text_I361-4244-3239-61541-2504-64002-1087-15453}>Button</p>
+<div className={styles.Button_361-4244}><div className={styles.-Button-Element-Base-Default_I361-4244-3239-61637}><div className={styles.text_I361-4244-3239-61637-2504-64001}><div className={styles.-Button-Element-Label_I361-4244-3239-61637-2504-64002}><p className={styles.text_I361-4244-3239-61637-2504-64002-1087-15453}>Button</p>
 </div></div></div></div>)}}
 export default Button_361-4244
