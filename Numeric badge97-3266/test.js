878888c328e6d6a10cfe20b53970f630f4ab0ca0
niamhcahillfugro <118794299+niamhcahillfugro@users.noreply.github.com>
2023-02-06 13:14:14 +0800

    Uploaded Numeric badge97-3266 component files

---
+++ Numeric badge97-3266/test.js
@@ -0,0 +1,7 @@
+import React, { Component } from 'react'
+import styles from './test.module.css'
+class Numeric_badge_495 extends Component {
+render() {return (
+<div className={styles.Numeric_badge_495}><p className={styles.1_817}>1</p>
+</div>)}}
+export default Numeric_badge_495
