03f53d020b2af3b4f28c97b0b242428ea392eab5
niamhcahillfugro <118794299+niamhcahillfugro@users.noreply.github.com>
2023-02-06 10:36:14 +0800

    testing update

---
+++ Badge_350-3785/Badge_350-3785.js
@@ -0,0 +1,7 @@
+import React, { Component } from 'react'
+import styles from './test.module.css'
+class Badge_350-3785 extends Component {
+render() {return (
+<div className={styles.Badge_350-3785}><div className={styles.-badge-master_I350-3785-2753-70325}><div className={styles.check_I350-3785-2753-70325-2753-70310}><div className={styles.Frame_I350-3785-2753-70325-2753-70310-5013-17291}></div></div><p className={styles.Badge_I350-3785-2753-70325-2753-70306}>Badge</p>
+</div></div>)}}
+export default Badge_350-3785
