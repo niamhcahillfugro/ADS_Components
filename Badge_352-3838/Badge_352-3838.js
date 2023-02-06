12423474165af98503252803a418b435981db09f
niamhcahillfugro <118794299+niamhcahillfugro@users.noreply.github.com>
2023-02-06 11:11:38 +0800

    uploading react file Badge_352-3838.js

--- Badge_352-3838/Badge_352-3838.js
+++ Badge_352-3838/Badge_352-3838.js
@@ -2,6 +2,6 @@ import React, { Component } from 'react'
 import styles from './test.module.css'
 class Badge_352-3838 extends Component {
 render() {return (
-<div className={styles.Badge_352-3838}><div className={styles.-badge-master_I352-3838-2753-70325}><div className={styles.check_I352-3838-2753-70325-2753-70310}><div className={styles.Frame_I352-3838-2753-70325-2753-70310-5013-17291}></div></div><p className={styles.Badge_I352-3838-2753-70325-2753-70306}>Badge</p>
+<div className={styles.Badge_352-3838}><div className={styles.-badge-master_I352-3838-2762-69087}><div className={styles.check_I352-3838-2762-69087-2753-70310}><div className={styles.Frame_I352-3838-2762-69087-2753-70310-5013-17291}></div></div><p className={styles.Badge_I352-3838-2762-69087-2753-70306}>Badge</p>
 </div></div>)}}
 export default Badge_352-3838
