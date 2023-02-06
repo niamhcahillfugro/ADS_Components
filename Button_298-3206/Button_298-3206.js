90f8035899b6298e4cebc8ec15feee53d40f5f27
niamhcahillfugro <118794299+niamhcahillfugro@users.noreply.github.com>
2023-02-06 10:02:53 +0800

    test

--- Button_298-3206/Button_298-3206.js
+++ Button_298-3206/Button_298-3206.js
@@ -3,5 +3,5 @@ import styles from './test.module.css'
 class Button_298-3206 extends Component {
 render() {return (
 <div className={styles.Button_298-3206}><div className={styles.-Button-Element-Base-Default_I298-3206-3239-61973}><div className={styles.text_I298-3206-3239-61973-2504-64019}><div className={styles.-Button-Element-Label_I298-3206-3239-61973-2504-64020}><p className={styles.text_I298-3206-3239-61973-2504-64020-1087-15453}>Button</p>
-</div></div><div className={styles.icon-right_I298-3206-3239-61973-2504-64023}><div className={styles.arrow-forward_I298-3206-3239-61973-2504-64024}><div className={styles.Frame_I298-3206-3239-61973-2504-64024-5013-16067}></div></div></div></div></div>)}}
+</div></div></div></div>)}}
 export default Button_298-3206
