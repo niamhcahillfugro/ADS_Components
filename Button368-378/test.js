18dcce0bcabd0c890c6b2275d4e1744b09e94be2
niamhcahillfugro <118794299+niamhcahillfugro@users.noreply.github.com>
2023-02-07 09:07:05 +0800

    Uploaded Button368-378 component files

--- Button368-378/test.js
+++ Button368-378/test.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import styles from './test.module.css'
-class Button_300 extends Component {
+class Button_543 extends Component {
 render() {return (
-<div className={styles.Button_300}><div className={styles._Button_/_Element_/_Base_Default_246}><div className={styles.icon_left_581}><div className={styles.favorite_402}><div className={styles.Frame_75}></div></div></div><div className={styles.text_288}><div className={styles._Button_/_Element_/_Label_854}><p className={styles.text_74}>Button</p>
-</div></div><div className={styles.icon_right_634}><div className={styles.arrow_forward_548}><div className={styles.Frame_634}></div></div></div></div></div>)}}
-export default Button_300
+<div className={styles.Button_543}><div className={styles.-Button-Element-Base-Default_556}><div className={styles.icon-left_32}><div className={styles.favorite_139}><div className={styles.Frame_664}></div></div></div><div className={styles.text_972}><div className={styles.-Button-Element-Label_34}><p className={styles.text_908}>Button</p>
+</div></div><div className={styles.icon-right_475}><div className={styles.arrow-forward_421}><div className={styles.Frame_436}></div></div></div></div></div>)}}
+export default Button_543
