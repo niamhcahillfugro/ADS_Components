00cc279d80f98d46efeae10116e9128c9619d696
niamhcahillfugro <n.cahill@fugro.com>
2023-02-07 12:10:40 +0800

    me refreshing

--- Button368-195/test.js
+++
@@ -1,7 +0,0 @@
-import React, { Component } from 'react'
-import styles from './test.module.css'
-class Button_363 extends Component {
-render() {return (
-<div className={styles.Button_363}><div className={styles._Button___Element___Base_Default_825}><div className={styles.icon_left_491}><div className={styles.favorite_299}><div className={styles.Frame_742}></div></div></div><div className={styles.text_20}><div className={styles._Button___Element___Label_749}><p className={styles.text_45}>Button</p>
-</div></div><div className={styles.icon_right_96}><div className={styles.arrow_forward_807}><div className={styles.Frame_401}></div></div></div></div></div>)}}
-export default Button_363
--- Numeric badge97-3266/test.js
+++
@@ -1,7 +0,0 @@
-import React, { Component } from 'react'
-import styles from './test.module.css'
-class Numeric_badge_215 extends Component {
-render() {return (
-<div className={styles.Numeric_badge_215}><p className={styles.x_293}>1</p>
-</div>)}}
-export default Numeric_badge_215
