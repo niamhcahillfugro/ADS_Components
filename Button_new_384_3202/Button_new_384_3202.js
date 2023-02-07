e6c2b45bce0e12ac21d1d9cd9e6bc7fa63383f62
niamhcahillfugro <118794299+niamhcahillfugro@users.noreply.github.com>
2023-02-07 11:30:53 +0800

    uploading react file Button_new_384_3202.js

--- Button_new_384_3202/Button_new_384_3202.js
+++ Button_new_384_3202/Button_new_384_3202.js
@@ -2,6 +2,6 @@ import React, { Component } from 'react'
 import styles from './Button_new_384_3202.css'
 class Button_new_384_3202 extends Component {
 render() {return (
-<div className={styles.Button_new_384_3202}><div className={styles._Button_Element_Base_Default_I384_3202_3239_61541}><div className={styles.text_I384_3202_3239_61541_2504_64001}><div className={styles._Button_Element_Label_I384_3202_3239_61541_2504_64002}><p className={styles.text_I384_3202_3239_61541_2504_64002_1087_15453}>Button</p>
+<div className={styles.Button_new_384_3202}><div className={styles.change_Button_Element_Base_Default_I384_3202_3239_61545}><div className={styles.text_I384_3202_3239_61545_2504_64001}><div className={styles._Button_Element_Label_I384_3202_3239_61545_2504_64002}><p className={styles.text_I384_3202_3239_61545_2504_64002_1087_15453}>Button</p>
 </div></div></div></div>)}}
 export default Button_new_384_3202
