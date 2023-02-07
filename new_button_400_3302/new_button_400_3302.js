822c4fe0343a889c9b06c0c4680958838b8370c8
niamhcahillfugro <118794299+niamhcahillfugro@users.noreply.github.com>
2023-02-07 16:28:10 +0800

    attempting tree upload

---
+++ new_button_400_3302/new_button_400_3302.js
@@ -0,0 +1,7 @@
+import React, { Component } from 'react'
+import styles from './new_button_400_3302.css'
+class new_button_400_3302 extends Component {
+render() {return (
+<div className={styles.new_button_400_3302}><div className={styles._Button_Element_Base_Default_I400_3302_3239_61541}><div className={styles.text_I400_3302_3239_61541_2504_64001}><div className={styles._Button_Element_Label_I400_3302_3239_61541_2504_64002}><p className={styles.text_I400_3302_3239_61541_2504_64002_1087_15453}>Button</p>
+</div></div></div></div>)}}
+export default new_button_400_3302
--- newbutton_400_3302/newbutton_400_3302.js
+++
@@ -1,7 +0,0 @@
-import React, { Component } from 'react'
-import styles from './newbutton_400_3302.css'
-class newbutton_400_3302 extends Component {
-render() {return (
-<div className={styles.newbutton_400_3302}><div className={styles._Button_Element_Base_Default_I400_3302_3239_61541}><div className={styles.text_I400_3302_3239_61541_2504_64001}><div className={styles._Button_Element_Label_I400_3302_3239_61541_2504_64002}><p className={styles.text_I400_3302_3239_61541_2504_64002_1087_15453}>Button</p>
-</div></div></div></div>)}}
-export default newbutton_400_3302
