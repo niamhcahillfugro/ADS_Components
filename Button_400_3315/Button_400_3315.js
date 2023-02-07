2b22bfbd81e6b62f2c0a05d8c4bd48ff095a0051
niamhcahillfugro <118794299+niamhcahillfugro@users.noreply.github.com>
2023-02-07 16:31:55 +0800

    attempting tree upload

--- Button_400_3315/Button_400_3315.js
+++ Button_400_3315/Button_400_3315.js
@@ -2,6 +2,6 @@ import React, { Component } from 'react'
 import styles from './Button_400_3315.css'
 class Button_400_3315 extends Component {
 render() {return (
-<div className={styles.Button_400_3315}><div className={styles._Button_Element_Base_Default_I400_3315_3239_61541}><div className={styles.text_I400_3315_3239_61541_2504_64001}><div className={styles._Button_Element_Label_I400_3315_3239_61541_2504_64002}><p className={styles.text_I400_3315_3239_61541_2504_64002_1087_15453}>Button</p>
+<div className={styles.Button_400_3315}><div className={styles._Button_Element_Base_Default_I400_3315_3239_61547}><div className={styles.text_I400_3315_3239_61547_2504_64001}><div className={styles._Button_Element_Label_I400_3315_3239_61547_2504_64002}><p className={styles.text_I400_3315_3239_61547_2504_64002_1087_15453}>Button</p>
 </div></div></div></div>)}}
 export default Button_400_3315
