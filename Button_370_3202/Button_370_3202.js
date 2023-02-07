911209247877ba6139971f1e17b7bdfefc5509c9
niamhcahillfugro <118794299+niamhcahillfugro@users.noreply.github.com>
2023-02-07 09:45:48 +0800

    uploading react file Button_370_3202.js

---
+++ Button_370_3202/Button_370_3202.js
@@ -0,0 +1,7 @@
+import React, { Component } from 'react'
+import styles from './Button_370_3202.css'
+class Button_370_3202 extends Component {
+render() {return (
+<div className={styles.Button_370_3202}><div className={styles._Button_Element_Base_Default_I370_3202_3239_61955}><div className={styles.text_I370_3202_3239_61955_2504_64055}><div className={styles._Button_Element_Label_I370_3202_3239_61955_2504_64056}><p className={styles.text_I370_3202_3239_61955_2504_64056_1087_15453}>Button</p>
+</div></div></div></div>)}}
+export default Button_370_3202
