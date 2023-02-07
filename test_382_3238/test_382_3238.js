44ea6a6ddd59e708da25ee503e307966c8dc6643
niamhcahillfugro <118794299+niamhcahillfugro@users.noreply.github.com>
2023-02-07 10:56:48 +0800

    uploading react file test_382_3238.js

---
+++ test_382_3238/test_382_3238.js
@@ -0,0 +1,7 @@
+import React, { Component } from 'react'
+import styles from './test_382_3238.css'
+class test_382_3238 extends Component {
+render() {return (
+<p className={styles.test_382_3238}>test</p>
+)}}
+export default test_382_3238
