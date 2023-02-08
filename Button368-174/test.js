48cdd402827ec2374156430b8b91efcbac78aceb
niamhcahillfugro <118794299+niamhcahillfugro@users.noreply.github.com>
2023-02-08 12:47:52 +0800

    Uploaded Button368-174 component files

---
+++ Button368-174/test.js
@@ -0,0 +1,7 @@
+import React, { Component } from 'react'
+import styles from './test.module.css'
+class Button_632 extends Component {
+render() {return (
+<div className={styles.Button_632}><div className={styles._Button___Element___Base_Default_13}><div className={styles.icon_left_154}><div className={styles.favorite_457}><div className={styles.Frame_156}></div></div></div><div className={styles.text_739}><div className={styles._Button___Element___Label_468}><p className={styles.text_825}>Button</p>
+</div></div><div className={styles.icon_right_796}><div className={styles.arrow_forward_483}><div className={styles.Frame_800}></div></div></div></div></div>)}}
+export default Button_632
