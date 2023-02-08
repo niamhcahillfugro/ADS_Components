568e1e6d7b25b9c8c7da1295beb7563e1b7e2651
niamhcahillfugro <118794299+niamhcahillfugro@users.noreply.github.com>
2023-02-08 11:11:15 +0800

    attempting tree upload

---
+++ Button_412_3202/Button_412_3202.js
@@ -0,0 +1,7 @@
+import React, { Component } from 'react'
+import styles from './Button_412_3202.css'
+class Button_412_3202 extends Component {
+render() {return (
+<div className={styles.Button_412_3202}><div className={styles._Button_Element_Base_Default_I412_3202_3239_61541}><div className={styles.text_I412_3202_3239_61541_2504_64001}><div className={styles._Button_Element_Label_I412_3202_3239_61541_2504_64002}><p className={styles.text_I412_3202_3239_61541_2504_64002_1087_15453}>Button</p>
+</div></div></div></div>)}}
+export default Button_412_3202
