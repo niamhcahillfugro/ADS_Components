17c6d03b76b44ebec7f7e66f1e6792823c5ee901
niamhcahillfugro <118794299+niamhcahillfugro@users.noreply.github.com>
2023-02-08 12:51:13 +0800

    attempting tree upload

---
+++ _Button_Element_Base_Default_I413_3227_3239_61541/_Button_Element_Base_Default_I413_3227_3239_61541.js
@@ -0,0 +1,7 @@
+import React, { Component } from 'react'
+import styles from './_Button_Element_Base_Default_I413_3227_3239_61541.css'
+class _Button_Element_Base_Default_I413_3227_3239_61541 extends Component {
+render() {return (
+<div className={styles._Button_Element_Base_Default_I413_3227_3239_61541}><div className={styles.text_I413_3227_3239_61541_2504_64001}><div className={styles._Button_Element_Label_I413_3227_3239_61541_2504_64002}><p className={styles.text_I413_3227_3239_61541_2504_64002_1087_15453}>Button</p>
+</div></div></div>)}}
+export default _Button_Element_Base_Default_I413_3227_3239_61541
