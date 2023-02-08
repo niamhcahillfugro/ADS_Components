2861782d7ebc54b1ad3587b04d558514b62b8b92
niamhcahillfugro <n.cahill@fugro.com>
2023-02-08 13:48:24 +0800

    me refreshing

--- Button_413_3227/Button_413_3227.js
+++
@@ -1,7 +0,0 @@
-import React, { Component } from 'react'
-import styles from './Button_413_3227.css'
-class Button_413_3227 extends Component {
-render() {return (
-<div className={styles.Button_413_3227}><div className={styles._Button_Element_Base_Default_I413_3227_3239_61541}><div className={styles.text_I413_3227_3239_61541_2504_64001}><div className={styles._Button_Element_Label_I413_3227_3239_61541_2504_64002}><p className={styles.text_I413_3227_3239_61541_2504_64002_1087_15453}>Button</p>
-</div></div></div></div>)}}
-export default Button_413_3227
--- Rectangle_1_112_3202/Rectangle_1_112_3202.js
+++
@@ -1,7 +0,0 @@
-import React, { Component } from 'react'
-import styles from './Rectangle_1_112_3202.css'
-class Rectangle_1_112_3202 extends Component {
-render() {return (
-<div className={styles.Rectangle_1_112_3202}></div>
-)}}
-export default Rectangle_1_112_3202
