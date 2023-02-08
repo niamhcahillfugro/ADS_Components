176b4bf47f6d06c73f5e33e4d995268c8664798d
Michael2001 <michael.simons2001@gmail.com>
2023-02-08 11:08:11 +0800

    init

--- Status_badge_106_69/Status_badge_106_69.js
+++
@@ -1,7 +0,0 @@
-import React, { Component } from 'react'
-import styles from './Status_badge_106_69.css'
-class Status_badge_106_69 extends Component {
-render() {return (
-<div className={styles.Status_badge_106_69}><div className={styles.base_I106_69_3471_67588}></div>
-</div>)}}
-export default Status_badge_106_69
