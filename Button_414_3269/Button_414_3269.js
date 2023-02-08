31fbda13f301c229b3a0c30214f4ba4ba97b4ddf
niamhcahillfugro <118794299+niamhcahillfugro@users.noreply.github.com>
2023-02-08 14:15:42 +0800

    attempting tree upload

--- Button_414_3269/Button_414_3269.js
+++ Button_414_3269/Button_414_3269.js
@@ -2,6 +2,6 @@ import React, { Component } from 'react'
 import styles from './Button_414_3269.css'
 class Button_414_3269 extends Component {
 render() {return (
-<div className={styles.Button_414_3269}><div className={styles._Button_Element_Base_Default_I414_3269_3239_61547}><div className={styles.text_I414_3269_3239_61547_2504_64001}><div className={styles._Button_Element_Label_I414_3269_3239_61547_2504_64002}><p className={styles.text_I414_3269_3239_61547_2504_64002_1087_15453}>Button</p>
+<div className={styles.Button_414_3269}><div className={styles._Button_Element_Base_Default_I414_3269_3239_61541}><div className={styles.text_I414_3269_3239_61541_2504_64001}><div className={styles._Button_Element_Label_I414_3269_3239_61541_2504_64002}><p className={styles.text_I414_3269_3239_61541_2504_64002_1087_15453}>Button</p>
 </div></div></div></div>)}}
 export default Button_414_3269
