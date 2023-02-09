f9e13785138997b94143880e582d51400c700219
niamhcahillfugro <n.cahill@fugro.com>
2023-02-09 08:47:38 +0800

    me refreshing

--- Button_414_3241/Button_414_3241.js
+++
@@ -1,7 +0,0 @@
-import React, { Component } from 'react'
-import styles from './Button_414_3241.css'
-class Button_414_3241 extends Component {
-render() {return (
-<div className={styles.Button_414_3241}><div className={styles._Button_Element_Base_Default_I414_3241_3239_61541}><div className={styles.text_I414_3241_3239_61541_2504_64001}><div className={styles._Button_Element_Label_I414_3241_3239_61541_2504_64002}><p className={styles.text_I414_3241_3239_61541_2504_64002_1087_15453}>Button</p>
-</div></div></div></div>)}}
-export default Button_414_3241
--- Button_418_3196/Button_418_3196.js
+++
@@ -1,7 +0,0 @@
-import React, { Component } from 'react'
-import styles from './Button_418_3196.css'
-class Button_418_3196 extends Component {
-render() {return (
-<div className={styles.Button_418_3196}><div className={styles._Button_Element_Base_Default_I418_3196_3239_61541}><div className={styles.text_I418_3196_3239_61541_2504_64001}><div className={styles._Button_Element_Label_I418_3196_3239_61541_2504_64002}><p className={styles.text_I418_3196_3239_61541_2504_64002_1087_15453}>Button</p>
-</div></div></div></div>)}}
-export default Button_418_3196
