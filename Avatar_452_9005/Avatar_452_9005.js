862854c9e3d0ca2cc9840a24990cbe5a815dc515
niamhcahillfugro <118794299+niamhcahillfugro@users.noreply.github.com>
2023-02-13 15:52:36 +0800

    Avatar_452_9005 content has been updated

--- Avatar_452_9005/Avatar_452_9005.js
+++ Avatar_452_9005/Avatar_452_9005.js
@@ -3,6 +3,6 @@ import styles from './Avatar_452_9005.css'
 class Avatar_452_9005 extends Component {
 render() {return (
 <div className={styles.Avatar_452_9005}><div className={styles.background_I452_9005_2123_33614}></div>
-<p className={styles.Initials_I452_9005_2123_33615}>MW</p>
+<p className={styles.Initials_I452_9005_2123_33615}>DIf</p>
 </div>)}}
 export default Avatar_452_9005
