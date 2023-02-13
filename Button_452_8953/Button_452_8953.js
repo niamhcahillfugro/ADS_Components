b0aac90d631748a3f380d99e1380410f18488827
niamhcahillfugro <118794299+niamhcahillfugro@users.noreply.github.com>
2023-02-13 15:39:39 +0800

    Button_452_8953 content has been updated

---
+++ Button_452_8953/Button_452_8953.js
@@ -0,0 +1,7 @@
+import React, { Component } from 'react'
+import styles from './Button_452_8953.css'
+class Button_452_8953 extends Component {
+render() {return (
+<div className={styles.Button_452_8953}><div className={styles._Label_Button_I452_8953_5748_152971}><div className={styles._Container_I452_8953_5748_152971_3239_61547}><div className={styles.text_I452_8953_5748_152971_3239_61547_2504_64001}><div className={styles._Label_I452_8953_5748_152971_3239_61547_2504_64002}><p className={styles.text_I452_8953_5748_152971_3239_61547_2504_64002_1087_15453}>Button</p>
+</div></div></div></div></div>)}}
+export default Button_452_8953
