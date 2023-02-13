24e916e0283ef12e1546b46cbe1795e10c1706e7
niamhcahillfugro <118794299+niamhcahillfugro@users.noreply.github.com>
2023-02-13 15:42:16 +0800

    Avatar_452_8948 content has been updated

---
+++ Avatar_452_8948/Avatar_452_8948.js
@@ -0,0 +1,8 @@
+import React, { Component } from 'react'
+import styles from './Avatar_452_8948.css'
+class Avatar_452_8948 extends Component {
+render() {return (
+<div className={styles.Avatar_452_8948}><div className={styles.background_I452_8948_2123_33614}></div>
+<p className={styles.Initials_I452_8948_2123_33615}>MW</p>
+</div>)}}
+export default Avatar_452_8948
