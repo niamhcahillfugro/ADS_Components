db58bed2b7d9c110a828ffdaa8d1f10c17bf606c
niamhcahillfugro <118794299+niamhcahillfugro@users.noreply.github.com>
2023-02-13 14:23:30 +0800

    Button_449_8719 content has been updated

---
+++ Button_449_8719/Button_449_8719.js
@@ -0,0 +1,7 @@
+import React, { Component } from 'react'
+import styles from './Button_449_8719.css'
+class Button_449_8719 extends Component {
+render() {return (
+<div className={styles.Button_449_8719}><div className={styles._Label_Button_I449_8719_5748_152971}><div className={styles._Container_I449_8719_5748_152971_3239_61547}><div className={styles.text_I449_8719_5748_152971_3239_61547_2504_64001}><div className={styles._Label_I449_8719_5748_152971_3239_61547_2504_64002}><p className={styles.text_I449_8719_5748_152971_3239_61547_2504_64002_1087_15453}>Button</p>
+</div></div></div></div></div>)}}
+export default Button_449_8719
