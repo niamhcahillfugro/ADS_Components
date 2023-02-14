c9057da5f41d1988aba927b582bc20c607edcafb
niamhcahillfugro <118794299+niamhcahillfugro@users.noreply.github.com>
2023-02-14 13:41:43 +0800

    Button_474_8713 content has been updated

---
+++ Button_474_8713/Button_474_8713.js
@@ -0,0 +1,6 @@
+import React, { Component } from 'react'
+import styles from './Button_474_8713.css'
+class Button_474_8713 extends Component {
+render() {return (
+[object Promise])}}
+export default Button_474_8713
