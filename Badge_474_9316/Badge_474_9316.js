727c6d18e5702dd1e90baec4f459945f2c899fc9
niamhcahillfugro <118794299+niamhcahillfugro@users.noreply.github.com>
2023-02-14 13:42:26 +0800

    Badge_474_9316 content has been updated

---
+++ Badge_474_9316/Badge_474_9316.js
@@ -0,0 +1,6 @@
+import React, { Component } from 'react'
+import styles from './Badge_474_9316.css'
+class Badge_474_9316 extends Component {
+render() {return (
+[object Promise])}}
+export default Badge_474_9316
--- Button_474_8713/Button_474_8713.js
+++
@@ -1,6 +0,0 @@
-import React, { Component } from 'react'
-import styles from './Button_474_8713.css'
-class Button_474_8713 extends Component {
-render() {return (
-[object Promise])}}
-export default Button_474_8713
