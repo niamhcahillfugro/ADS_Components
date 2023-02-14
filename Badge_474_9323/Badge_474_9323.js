4161177eb787a955bca5c33297d61319349af25b
niamhcahillfugro <118794299+niamhcahillfugro@users.noreply.github.com>
2023-02-14 14:05:09 +0800

    Badge_474_9323 content has been updated

---
+++ Badge_474_9323/Badge_474_9323.js
@@ -0,0 +1,6 @@
+import React, { Component } from 'react'
+import styles from './Badge_474_9323.css'
+class Badge_474_9323 extends Component {
+render() {return (
+[object Promise])}}
+export default Badge_474_9323
