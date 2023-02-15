3d49aedf400d7880279a5681586c3f44fac3e61f
niamhcahillfugro <118794299+niamhcahillfugro@users.noreply.github.com>
2023-02-15 12:11:25 +0800

    Badge_474_9323 content has been updated

--- Badge_474_9323/Badge_474_9323.js
+++ Badge_474_9323/Badge_474_9323.js
@@ -2,6 +2,5 @@ import React, { Component } from 'react'
 import styles from './Badge_474_9323.css'
 class Badge_474_9323 extends Component {
 render() {return (
-<div className={styles.Badge_474_9323}><div className={styles._badge_master_I474_9323_2753_70325}><div className={styles.check_I474_9323_2753_70325_2753_70310}><div className={styles.Frame_I474_9323_2753_70325_2753_70310_5013_17291}></div></div><p className={styles.Badge_I474_9323_2753_70325_2753_70306}>Badge</p>
-</div></div>)}}
+[object Promise])}}
 export default Badge_474_9323
