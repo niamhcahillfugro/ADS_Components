8125b5e184758d1a945f1b14e64f6a0534897c04
Michael2001 <michael.simons2001@gmail.com>
2023-02-15 14:43:44 +0800

    Button_486_9064 content has been updated

---
+++ Button_486_9064/Button_486_9064.js
@@ -0,0 +1,7 @@
+import React, { Component } from 'react'
+import styles from './Button_486_9064.css'
+class Button_486_9064 extends Component {
+render() {return (
+<div className={styles.Button_486_9064}><div className={styles._Label_Button_I486_9064_5748_152971}><div className={styles._Container_I486_9064_5748_152971_3239_61547}><div className={styles.text_I486_9064_5748_152971_3239_61547_2504_64001}><div className={styles._Label_I486_9064_5748_152971_3239_61547_2504_64002}><p className={styles.text_I486_9064_5748_152971_3239_61547_2504_64002_1087_15453}>Button</p>
+</div></div></div></div></div>)}}
+export default Button_486_9064
