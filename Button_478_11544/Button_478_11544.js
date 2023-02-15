bf9891d4812542a9a26109ee7f94a34ad89b96b3
niamhcahillfugro <118794299+niamhcahillfugro@users.noreply.github.com>
2023-02-15 10:35:56 +0800

    Button_478_11544 content has been updated

---
+++ Button_478_11544/Button_478_11544.js
@@ -0,0 +1,6 @@
+import React, { Component } from 'react'
+import styles from './Button_478_11544.css'
+class Button_478_11544 extends Component {
+render() {return (
+[object Promise])}}
+export default Button_478_11544
