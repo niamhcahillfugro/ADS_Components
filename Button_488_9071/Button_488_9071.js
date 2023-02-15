0bb3cd65df99c61f6a43150a2b5f357ba3457097
Michael2001 <michael.simons2001@gmail.com>
2023-02-15 15:16:31 +0800

    Button_488_9071 content has been updated

---
+++ Button_488_9071/Button_488_9071.js
@@ -0,0 +1,7 @@
+import React, { Component } from 'react'
+import styles from './Button_488_9071.css'
+class Button_488_9071 extends Component {
+render() {return (
+<div className={styles.Button_488_9071}><div className={styles._Label_Button_I488_9071_5748_152971}><div className={styles._Container_I488_9071_5748_152971_3239_61547}><div className={styles.text_I488_9071_5748_152971_3239_61547_2504_64001}><div className={styles._Label_I488_9071_5748_152971_3239_61547_2504_64002}><p className={styles.text_I488_9071_5748_152971_3239_61547_2504_64002_1087_15453}>Button</p>
+</div></div></div></div></div>)}}
+export default Button_488_9071
