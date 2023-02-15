951a770f86594cd80b0ecc6c3d0296c02089df43
Michael2001 <michael.simons2001@gmail.com>
2023-02-15 11:03:31 +0800

    Cards_162_9712 content has been updated

--- Button_157_11220/Button_157_11220.js
+++
@@ -1,7 +0,0 @@
-import React, { Component } from 'react'
-import styles from './Button_157_11220.css'
-class Button_157_11220 extends Component {
-render() {return (
-<div className={styles.Button_157_11220}><div className={styles._Label_Button_I157_11220_5748_152971}><div className={styles._Container_I157_11220_5748_152971_3239_61547}><div className={styles.text_I157_11220_5748_152971_3239_61547_2504_64001}><div className={styles._Label_I157_11220_5748_152971_3239_61547_2504_64002}><p className={styles.text_I157_11220_5748_152971_3239_61547_2504_64002_1087_15453}>Button</p>
-</div></div></div></div></div>)}}
-export default Button_157_11220
