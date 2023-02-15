9dbd12b5ab3e79e2ff04d4d62b4eca5409ae665e
Michael2001 <michael.simons2001@gmail.com>
2023-02-15 12:08:54 +0800

    Button_164_11210 content has been updated

---
+++ Button_164_11210/Button_164_11210.js
@@ -0,0 +1,7 @@
+import React, { Component } from 'react'
+import styles from './Button_164_11210.css'
+class Button_164_11210 extends Component {
+render() {return (
+<div className={styles.Button_164_11210}><div className={styles._Label_Button_I164_11210_5748_152971}><div className={styles._Container_I164_11210_5748_152971_3239_61547}><div className={styles.text_I164_11210_5748_152971_3239_61547_2504_64001}><div className={styles._Label_I164_11210_5748_152971_3239_61547_2504_64002}><p className={styles.text_I164_11210_5748_152971_3239_61547_2504_64002_1087_15453}>Button</p>
+</div></div></div></div></div>)}}
+export default Button_164_11210
