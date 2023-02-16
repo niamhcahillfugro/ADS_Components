471a812d9ed16d7b62ed5f323ff05d83897dcfee
niamhcahillfugro <118794299+niamhcahillfugro@users.noreply.github.com>
2023-02-16 11:38:13 +0800

    Button_511_11556 content has been updated

---
+++ Button_511_11556/Button_511_11556.js
@@ -0,0 +1,7 @@
+import React, { Component } from 'react'
+import styles from './Button_511_11556.css'
+class Button_511_11556 extends Component {
+render() {return (
+<div className={styles.Button_511_11556}><div className={styles._Label_Button_I511_11556_5748_152971}><div className={styles._Container_I511_11556_5748_152971_3239_61547}><div className={styles.text_I511_11556_5748_152971_3239_61547_2504_64001}><div className={styles._Label_I511_11556_5748_152971_3239_61547_2504_64002}><p className={styles.text_I511_11556_5748_152971_3239_61547_2504_64002_1087_15453}>Button</p>
+</div></div></div></div></div>)}}
+export default Button_511_11556
