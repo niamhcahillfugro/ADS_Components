1fa3126c8b5cf12d3a87747be2bf901dd08955f7
niamhcahillfugro <118794299+niamhcahillfugro@users.noreply.github.com>
2023-02-16 15:32:05 +0800

    Avatar_515_9212 content has been updated

---
+++ Avatar_515_9212/Avatar_515_9212.js
@@ -0,0 +1,8 @@
+import React, { Component } from 'react'
+import styles from './Avatar_515_9212.css'
+class Avatar_515_9212 extends Component {
+render() {return (
+<div className={styles.Avatar_515_9212}><div className={styles.background_I515_9212_2123_33614}></div>
+<p className={styles.Initials_I515_9212_2123_33615}>MW</p>
+</div>)}}
+export default Avatar_515_9212
