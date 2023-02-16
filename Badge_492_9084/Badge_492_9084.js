04ce88e43b8133bac55f48bbb8b53b0e460a26a2
Michael2001 <michael.simons2001@gmail.com>
2023-02-16 08:49:54 +0800

    Badge_492_9084 content has been updated

---
+++ Badge_492_9084/Badge_492_9084.js
@@ -0,0 +1,7 @@
+import React, { Component } from 'react'
+import styles from './Badge_492_9084.css'
+class Badge_492_9084 extends Component {
+render() {return (
+<div className={styles.Badge_492_9084}><div className={styles._badge_master_I492_9084_2753_70325}><div className={styles.check_I492_9084_2753_70325_2753_70310}><div className={styles.Frame_I492_9084_2753_70325_2753_70310_5013_17291}></div></div><p className={styles.Badge_I492_9084_2753_70325_2753_70306}>Badge</p>
+</div></div>)}}
+export default Badge_492_9084
