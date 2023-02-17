62b52296a338f7642f56a129946cc56e00d1ef10
niamhcahillfugro <118794299+niamhcahillfugro@users.noreply.github.com>
2023-02-17 11:49:15 +0800

    Avatar_529_9092 content has been updated

---
+++ Avatar_529_9092/Avatar_529_9092.js
@@ -0,0 +1,8 @@
+import React, { Component } from 'react'
+import styles from './Avatar_529_9092.css'
+class Avatar_529_9092 extends Component {
+render() {return (
+<div className={styles.Avatar_529_9092}><div className={styles.background_I529_9092_2123_33614}></div>
+<p className={styles.Initials_I529_9092_2123_33615}>MW</p>
+</div>)}}
+export default Avatar_529_9092
