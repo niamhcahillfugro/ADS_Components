c0e1220b56b7d5b163f777ede048c86e049d668f
niamhcahillfugro <118794299+niamhcahillfugro@users.noreply.github.com>
2023-02-17 09:02:07 +0800

    Button_520_11230 content has been updated

---
+++ Button_520_11230/Button_520_11230.js
@@ -0,0 +1,7 @@
+import React, { Component } from 'react'
+import styles from './Button_520_11230.css'
+class Button_520_11230 extends Component {
+render() {return (
+<div className={styles.Button_520_11230}><div className={styles._Label_Button_I520_11230_5748_152971}><div className={styles._Container_I520_11230_5748_152971_3239_61547}><div className={styles.text_I520_11230_5748_152971_3239_61547_2504_64001}><div className={styles._Label_I520_11230_5748_152971_3239_61547_2504_64002}><p className={styles.text_I520_11230_5748_152971_3239_61547_2504_64002_1087_15453}>Button</p>
+</div></div></div></div></div>)}}
+export default Button_520_11230
