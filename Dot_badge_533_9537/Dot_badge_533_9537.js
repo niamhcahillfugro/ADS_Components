b0a89778dc3e7ab89c036556abe379da1afad210
niamhcahillfugro <118794299+niamhcahillfugro@users.noreply.github.com>
2023-02-17 13:51:03 +0800

    Dot_badge_533_9537 content has been updated

---
+++ Dot_badge_533_9537/Dot_badge_533_9537.js
@@ -0,0 +1,7 @@
+import React, { Component } from 'react'
+import styles from './Dot_badge_533_9537.css'
+class Dot_badge_533_9537 extends Component {
+render() {return (
+<div className={styles.Dot_badge_533_9537}><div className={styles.dot_I533_9537_2556_67612}></div>
+</div>)}}
+export default Dot_badge_533_9537
