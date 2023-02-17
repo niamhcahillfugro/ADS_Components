66284aff1b39c10749b9b1f6aafe92b176965d54
niamhcahillfugro <118794299+niamhcahillfugro@users.noreply.github.com>
2023-02-17 13:46:37 +0800

    Avatar_group_533_9486 content has been updated

---
+++ Avatar_group_533_9486/Avatar_group_533_9486.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+import styles from './Avatar_group_533_9486.css'
+class Avatar_group_533_9486 extends Component {
+render() {return (
+<div className={styles.Avatar_group_533_9486}><div className={styles.Avatar_I533_9486_2123_38440}><div className={styles.background_I533_9486_2123_38440_2123_33614}></div>
+<p className={styles.Initials_I533_9486_2123_38440_2123_33615}>MW</p>
+<div className={styles.image_I533_9486_2123_38440_2123_33616}></div></div><div className={styles.Avatar_I533_9486_2123_37770}><div className={styles.background_I533_9486_2123_37770_2123_33614}></div>
+<p className={styles.Initials_I533_9486_2123_37770_2123_33615}>MW</p>
+<div className={styles.image_I533_9486_2123_37770_2123_33616}></div></div><div className={styles.Avatar_I533_9486_2123_38434}><div className={styles.background_I533_9486_2123_38434_2123_33614}></div>
+<p className={styles.Initials_I533_9486_2123_38434_2123_33615}>MW</p>
+<div className={styles.image_I533_9486_2123_38434_2123_33616}></div></div><div className={styles.Avatar_I533_9486_2123_33727}><div className={styles.background_I533_9486_2123_33727_2123_33614}></div>
+<p className={styles.Initials_I533_9486_2123_33727_2123_33615}>MW</p>
+<div className={styles.image_I533_9486_2123_33727_2123_33616}></div></div><div className={styles.Avatar_I533_9486_2123_39138}><div className={styles.background_I533_9486_2123_39138_2123_33614}></div>
+<p className={styles.Initials_I533_9486_2123_39138_2123_33615}>+2</p>
+</div></div>)}}
+export default Avatar_group_533_9486
