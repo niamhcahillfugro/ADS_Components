cafe1e072361fbbfc6a68ee994a3ac31c0bae90c
niamhcahillfugro <118794299+niamhcahillfugro@users.noreply.github.com>
2023-02-17 13:45:00 +0800

    Line_1_533_9109 content has been updated

---
+++ Line_1_533_9109/Line_1_533_9109.js
@@ -0,0 +1,7 @@
+import React, { Component } from 'react'
+import styles from './Line_1_533_9109.css'
+class Line_1_533_9109 extends Component {
+render() {return (
+<div className={styles.Line_1_533_9109}></div>
+)}}
+export default Line_1_533_9109
--- Rectangle_1_532_9107/Rectangle_1_532_9107.js
+++
@@ -1,7 +0,0 @@
-import React, { Component } from 'react'
-import styles from './Rectangle_1_532_9107.css'
-class Rectangle_1_532_9107 extends Component {
-render() {return (
-<div className={styles.Rectangle_1_532_9107}></div>
-)}}
-export default Rectangle_1_532_9107
