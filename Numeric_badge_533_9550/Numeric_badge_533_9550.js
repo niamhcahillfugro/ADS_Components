2c35a789ddd67ab854fbf3ea10ea6a3842b72951
niamhcahillfugro <118794299+niamhcahillfugro@users.noreply.github.com>
2023-02-17 14:21:47 +0800

    Numeric_badge_533_9550 content has been updated

--- Numeric_badge_533_9550/Numeric_badge_533_9550.js
+++ Numeric_badge_533_9550/Numeric_badge_533_9550.js
@@ -2,6 +2,6 @@ import React, { Component } from 'react'
 import styles from './Numeric_badge_533_9550.css'
 class Numeric_badge_533_9550 extends Component {
 render() {return (
-<div className={styles.Numeric_badge_533_9550}><p className={styles.1_I533_9550_2768_68938}>1</p>
+<div className={styles.Numeric_badge_533_9550}><p className={styles.Number_I533_9550_2768_68938}>1</p>
 </div>)}}
 export default Numeric_badge_533_9550
