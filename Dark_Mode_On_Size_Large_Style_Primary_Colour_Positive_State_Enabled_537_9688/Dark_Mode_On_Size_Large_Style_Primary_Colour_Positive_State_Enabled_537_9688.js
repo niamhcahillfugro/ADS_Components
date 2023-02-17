17dc6932bad331ccd539f524343d01a0ff90648d
niamhcahillfugro <118794299+niamhcahillfugro@users.noreply.github.com>
2023-02-17 16:08:12 +0800

    Dark_Mode_On_Size_Large_Style_Primary_Colour_Positive_State_Enabled_537_9688 content has been updated

---
+++ Dark_Mode_On_Size_Large_Style_Primary_Colour_Positive_State_Enabled_537_9688/Dark_Mode_On_Size_Large_Style_Primary_Colour_Positive_State_Enabled_537_9688.js
@@ -0,0 +1,7 @@
+import React, { Component } from 'react'
+import styles from './Dark_Mode_On_Size_Large_Style_Primary_Colour_Positive_State_Enabled_537_9688.css'
+class Dark_Mode_On_Size_Large_Style_Primary_Colour_Positive_State_Enabled_537_9688 extends Component {
+render() {return (
+<div className={styles.Dark_Mode_On_Size_Large_Style_Primary_Colour_Positive_State_Enabled_537_9688}><div className={styles._Container_537_9689}><div className={styles.text_I537_9689_2504_64001}><div className={styles._Label_I537_9689_2504_64002}><p className={styles.text_I537_9689_2504_64002_1087_15453}>Button</p>
+</div></div></div></div>)}}
+export default Dark_Mode_On_Size_Large_Style_Primary_Colour_Positive_State_Enabled_537_9688
