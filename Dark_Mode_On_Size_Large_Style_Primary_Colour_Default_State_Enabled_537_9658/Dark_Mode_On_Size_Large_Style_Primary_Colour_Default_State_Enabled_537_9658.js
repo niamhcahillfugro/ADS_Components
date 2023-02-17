2e1d74fce7f453cd42adb2dfb88d8e76f22f4a6f
niamhcahillfugro <118794299+niamhcahillfugro@users.noreply.github.com>
2023-02-17 16:04:31 +0800

    Dark_Mode_On_Size_Large_Style_Primary_Colour_Default_State_Enabled_537_9658 content has been updated

---
+++ Dark_Mode_On_Size_Large_Style_Primary_Colour_Default_State_Enabled_537_9658/Dark_Mode_On_Size_Large_Style_Primary_Colour_Default_State_Enabled_537_9658.js
@@ -0,0 +1,7 @@
+import React, { Component } from 'react'
+import styles from './Dark_Mode_On_Size_Large_Style_Primary_Colour_Default_State_Enabled_537_9658.css'
+class Dark_Mode_On_Size_Large_Style_Primary_Colour_Default_State_Enabled_537_9658 extends Component {
+render() {return (
+<div className={styles.Dark_Mode_On_Size_Large_Style_Primary_Colour_Default_State_Enabled_537_9658}><div className={styles._Container_537_9659}><div className={styles.text_I537_9659_2504_64001}><div className={styles._Label_I537_9659_2504_64002}><p className={styles.text_I537_9659_2504_64002_1087_15453}>Button</p>
+</div></div></div></div>)}}
+export default Dark_Mode_On_Size_Large_Style_Primary_Colour_Default_State_Enabled_537_9658
