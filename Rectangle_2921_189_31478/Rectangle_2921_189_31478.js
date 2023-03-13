725e1198188fb0e38b3c278ad5d6406a4e2a1ffe
niamhcahillfugro <118794299+niamhcahillfugro@users.noreply.github.com>
2023-03-13 14:11:32 +0800

    Rectangle_2921_189_31478 content has been updated

---
+++ Rectangle_2921_189_31478/Rectangle_2921_189_31478.js
@@ -0,0 +1,7 @@
+import React, { Component } from 'react'
+import styles from './Rectangle_2921_189_31478.css'
+class Rectangle_2921_189_31478 extends Component {
+render() {return (
+<div className={styles.Rectangle_2921_189_31478}><div className={styles.Rectangle_2921_189_31477}></div>
+</div>)}}
+export default Rectangle_2921_189_31478
