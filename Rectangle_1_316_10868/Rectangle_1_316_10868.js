2876893e390f0b43db7e9123f311f1da7935b7e1
nikola-gopcevic <118794297+nikola-gopcevic@users.noreply.github.com>
2023-03-14 13:21:17 +0800

    Rectangle_1_316_10868 content has been updated

---
+++ Rectangle_1_316_10868/Rectangle_1_316_10868.js
@@ -0,0 +1,7 @@
+import React, { Component } from 'react'
+import styles from './Rectangle_1_316_10868.css'
+class Rectangle_1_316_10868 extends Component {
+render() {return (
+<div className='Rectangle_1_316_10868'><div className='Rectangle_1_316_10867'></div>
+</div>)}}
+export default Rectangle_1_316_10868
