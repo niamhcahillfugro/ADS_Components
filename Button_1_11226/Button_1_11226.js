bd0ce7561d5d0ecbdeff1e8297576d9664d4ab29
Michael2001 <michael.simons2001@gmail.com>
2023-04-17 15:25:36 +0800

    Button_1_11226 content has been updated

---
+++ Button_1_11226/Button_1_11226.js
@@ -0,0 +1,7 @@
+import React, { Component } from 'react'
+import styles from './Button_1_11226.css'
+class Button_1_11226 extends Component {
+render() {return (
+<div className='Button_1_11226'><div className='Button_1_11212'><div className='_Label_Button_I1_11212_5748_152971'><div className='_Content_I1_11212_5748_152971_3239_61547'><div className='Label_I1_11212_5748_152971_3239_61547_2504_64001'><div className='_Label_I1_11212_5748_152971_3239_61547_2504_64002'><p className='Label_I1_11212_5748_152971_3239_61547_2504_64002_1087_15453'>Button</p>
+</div></div></div></div></div></div>)}}
+export default Button_1_11226
